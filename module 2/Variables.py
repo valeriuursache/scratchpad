c02ff45abb277c85132a4af7492dbff2058911d1
valeriuursache <valeriuursache@users.noreply.github.com>
2021-02-28 20:34:09 -0800

    Create Variables.py

---
+++ module 2/Variables.py
@@ -0,0 +1,12 @@
+username = "dwbhappy"
+password = 12345
+print(str(password))
+print(type(str(password)))
+print(username + str(password))
+
+
+changed_password = password + 54321
+print (changed_password)
+
+
+           
