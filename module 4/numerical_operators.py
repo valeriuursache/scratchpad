ee84d5f0975268622b56b6c56bbc8f6cdcf95ba7
valeriuursache <valeradance94@gmail.com>
2021-03-14 21:04:42 -0700

    Create numerical_operators.py

---
+++ module 4/numerical_operators.py
@@ -0,0 +1,36 @@
+
+
+if __name__ =="__main__":
+
+
+        #addition
+        result = 3 + 9
+        result = -3 + 4.0
+        # Subtraction
+        result = -3 - 4.0
+        result = -10.0 - 4
+        # Multiplication
+        result = 4 * 3
+        result = 6 * -2.1
+        # Division
+        result = 4 / 3 
+        result = 8 / 3.0
+
+        # Integer Division
+        result = 4 // 3
+
+        # Modulus (Remainder)
+        reaminder = 4 % 3
+
+        # result = 5 / 0
+
+        # Exponentiation
+         
+    result = 9 ** 0.5
+    result = 27 ** (1 / 3)
+
+
+
+
+
+        print("script complete")
