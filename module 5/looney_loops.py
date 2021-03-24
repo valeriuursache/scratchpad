3bce5f1b025af08e55be75686f1dc3c18600fd48
valeriuursache <valeradance94@gmail.com>
2021-03-23 23:20:35 -0700

    Update looney_loops.py

--- module 5/looney_loops.py
+++ module 5/looney_loops.py
@@ -1,7 +1,44 @@
 if __name__ == "__main__":
 
-    vowels = ('a', 'e', 'i', 'o', 'u')
-    for letter in vowels:
+    # tuple pokemon
+    pokemon = ('pikachu', 'charmander', 'bulbasaur')
+    print(pokemon[1])
+    starter1 = 'pikachu'
+    starter2 = 'charmander'
+    starter3 = 'bulbasaur'
+    
+    # tuple letters
+    my_name = ('v', 'a', 'l', 'e', 'r', 'i','u')
+    print('i' in my_name)
+
+    # for loop
+    for i in range(2, 11):
+        print(i)
+    # while loop
+    i = 2
+    while(i<=10):
+        print(i)
+        i += 1 
+    # for loop string
+    simple_string = "This is a simple string"
+    for letter in simple_string:
         print(letter)
-        print('script complete')
+
+    # nested loop
+    simple_set = ('this', 'is', 'a', 'simple', 'set')
+    for i in range(len(simple_set)):
+        print(simple_set[i])
+        print(simple_set[i])
+        print(simple_set[i])
+
         
+    
+    
+          
+    
+        
+
+
+
+    
+    
