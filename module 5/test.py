f692cb3d4fb9389a65ce4b4cdf3b2cbf81ddf150
valeriuursache <valeradance94@gmail.com>
2021-03-23 20:09:05 -0700

    Create test.py

---
+++ module 5/test.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+
+
+    pokemon_type_pairs = {
+    ('Pikachu', 'electric'),
+    ('Bulbasaur', 'grass'),
+    ('Charmander', 'fire')
+}
+    print(pokemon_types.get('Eevee'))
+
+  
